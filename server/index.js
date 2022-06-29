7638bc64ab939c715cf85f3399c322164d159f5f
Maginele Larins <100315803ml@gmail.com>
2022-06-29 14:35:56 -0600

    initial commit

--- server/index.js
+++ server/index.js
@@ -2,3 +2,15 @@ const express= require('express')
 const path =require ('path')
 
 const app = express()
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.js'))
+  })
+
+
+  const port =  process.env.PORT || 4004
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
