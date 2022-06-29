467ae06e6dc09f1e1b218aa08db78e33c8ebe43a
Maginele Larins <100315803ml@gmail.com>
2022-06-29 15:29:31 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const bigButton = document.querySelector('')
+const message = () => {
+       alert('Reserve your spot!')
+}
+bigButton.addEventListener('click', message)
--- server/index.js
+++ server/index.js
@@ -7,6 +7,12 @@ const app = express()
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
   })
+  app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, '../styles.css'))
+})
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.js'))
+})
 
 
   const port =  process.env.PORT || 4004
